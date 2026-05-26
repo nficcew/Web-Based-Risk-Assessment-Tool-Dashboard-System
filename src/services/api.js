0b101d6644baf08f812a15f7099095520fc8048f
Nafisah <saidatunnfsh02@gmail.com>
2026-05-26 23:28:51 +0800

    fix API URL for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 // API Base URL
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL 
+  ? `${import.meta.env.VITE_API_URL}/api` 
+  : 'http://localhost:5000/api';
 
 // Create axios instance
 const api = axios.create({
